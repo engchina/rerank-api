6b962d6c18b0d63af11980facc8fad25d83d7802
engchina <engchina>
2024-06-16 13:13:49 +0800

    update

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-bge_reranker_v2_minicpm_layerwise = LayerWiseFlagLLMReranker('BAAI/bge-reranker-v2-minicpm-layerwise', use_fp16=True)
+# bge_reranker_v2_minicpm_layerwise = LayerWiseFlagLLMReranker('BAAI/bge-reranker-v2-minicpm-layerwise', use_fp16=True)
 # bge_reranker_v2_gemma = FlagLLMReranker('BAAI/bge-reranker-v2-gemma', use_fp16=True)
-bge_reranker_v2_m3 = FlagReranker('BAAI/bge-reranker-v2-m3', use_fp16=True)
+# bge_reranker_v2_m3 = FlagReranker('BAAI/bge-reranker-v2-m3', use_fp16=True)
 bce_reranker_base_v1 = RerankerModel(model_name_or_path="maidalun1020/bce-reranker-base_v1")
 
 
@@ -31,6 +31,8 @@ def rerank_docs(manager: DocumentRankerManager) -> list:
     ranker_model = manager.ranker_model
     unranked_docs = manager.unranked_docs
     if ranker_model == 'BAAI/bge-reranker-v2-minicpm-layerwise':
+        bge_reranker_v2_minicpm_layerwise = LayerWiseFlagLLMReranker('BAAI/bge-reranker-v2-minicpm-layerwise',
+                                                                     use_fp16=True)
         cross = [(query_text, doc) for doc in unranked_docs]
         ce_scores = bge_reranker_v2_minicpm_layerwise.compute_score(cross, batch_size=1, cutoff_layers=[28])
         return ce_scores
@@ -38,15 +40,16 @@ def rerank_docs(manager: DocumentRankerManager) -> list:
     #     cross = [(query_text, doc) for doc in unranked_docs]
     #     ce_scores = bge_reranker_v2_gemma.compute_score(cross, batch_size=1)
     #     return ce_scores
-    elif ranker_model == 'BAAI/bge-reranker-v2-m3':
-        cross = [(query_text, doc) for doc in unranked_docs]
-        ce_scores = bge_reranker_v2_m3.compute_score(cross, batch_size=1)
-        return ce_scores
+    # elif ranker_model == 'BAAI/bge-reranker-v2-m3':
+    #     cross = [(query_text, doc) for doc in unranked_docs]
+    #     ce_scores = bge_reranker_v2_m3.compute_score(cross, batch_size=1)
+    #     return ce_scores
     elif ranker_model == 'maidalun1020/bce-reranker-base_v1':
         cross = [(query_text, doc) for doc in unranked_docs]
         ce_scores = bce_reranker_base_v1.compute_score(cross, batch_size=1)
         return ce_scores
     else:
+        bge_reranker_v2_m3 = FlagReranker('BAAI/bge-reranker-v2-m3', use_fp16=True)
         cross = [(query_text, doc) for doc in unranked_docs]
         ce_scores = bge_reranker_v2_m3.compute_score(cross, batch_size=1, cutoff_layers=[28])
         return ce_scores

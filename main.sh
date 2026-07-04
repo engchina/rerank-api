8ac87c311fa7f4a9f4d2e230a99a8c7a0f13778d
root <root@DESKTOP-31CKLQ1>
2026-07-04 20:48:16 +0800

    update

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 eval "$(conda shell.bash hook)"
 conda activate rerank-api
-PYTORCH_NVML_BASED_CUDA_CHECK=-1 CUDA_VISIBLE_DEVICES=2 uvicorn openai_api:app --reload --host 0.0.0.0 --port 8886
+uvicorn openai_api:app --reload --host 0.0.0.0 --port 8886
